845ba4a47f1b4620c4368f45ca268bf17c8c8ec6
HilalKaratay <56538589+HilalKaratay@users.noreply.github.com>
2023-04-27 23:29:13 +0200

    Klassen angelegt und erstes Frame angefangen

--- app/src/main/java/com/example/mocopraktikum23/ProfilAnlegen.kt
+++ app/src/main/java/com/example/mocopraktikum23/ProfilAnlegen.kt
@@ -1,19 +1,15 @@
 package com.example.mocopraktikum23
 
 import android.os.Bundle
-import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mocopraktikum23.ui.theme.MocoPraktikum23Theme
 
-class MainActivity : ComponentActivity() {
+class ProfilAnlegen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,26 +19,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
                 }
             }
-        }
-    }
-
-
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        MocoPraktikum23Theme {
-            Greeting("Android")
         }
     }
 }
--- app/src/main/java/com/example/mocopraktikum23/Profilseite.kt
+++ app/src/main/java/com/example/mocopraktikum23/Profilseite.kt
@@ -3,13 +3,21 @@ package com.example.mocopraktikum23
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mocopraktikum23.ui.theme.MocoPraktikum23Theme
 
 class Profilseite : ComponentActivity() {
@@ -17,12 +25,13 @@ class Profilseite : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MocoPraktikum23Theme {
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greetings("Android")
+
                 }
             }
         }
@@ -31,17 +40,45 @@ class Profilseite : ComponentActivity() {
 
 
 @Composable
-fun Greetings(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TravelBuddiesLeiste(name: String, modifier: Modifier = Modifier) {
+    Column(
+
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.End
+    ) {
+        Text(
+            text = "TravelBuddies", fontSize = 33.sp,
+            modifier = Modifier
+                .padding(
+                    top = 16.dp, end = 115.dp,
+                )
+        )
+
+        Text(
+            text = "Profil",
+            fontSize = 29.sp,
+            modifier = Modifier
+                .padding(
+                  end = 110.dp
+                )
+        )
+    }
+}
+
+@Composable
+fun PersonenDaten(name: String,modifier: Modifier){
+     Box(modifier = ) {
+        
+    }
+    
+    
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingsPreview() {
+fun TravelBuddiesLeistePreview() {
     MocoPraktikum23Theme {
-        Greeting("Android")
+        TravelBuddiesLeiste("Travel")
     }
 }
